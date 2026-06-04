6221f3c2f3e7083c6ee4b154734311412eb01bfb
Dmitriy Liakhovets <dmitriyliakhovets@MacBook-Air-Dmitriy.local>
2026-06-04 21:02:26 +0200

    Сделана регистрация

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,11 +1,14 @@
 from aiogram import Router, F, html
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types.web_app_info import WebAppInfo
 
 from sqlalchemy import select
 from bot.keyboards import get_main_keyboard
 from core.database import async_session_maker
 from core.models import User
+from core.config import settings
 
 start_router = Router()
 
@@ -26,10 +29,15 @@ async def cmd_start(message: Message):
             session.add(new_user)
             await session.commit()
             
+    # Создаем инлайн-клавиатуру (кнопка, прикрепленная к сообщению)
+    inline_kb = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="🛍 Открыть магазин", web_app=WebAppInfo(url=settings.mini_app_url))]
+    ])
+
     await message.answer(
         f"Привет, {html.bold(message.from_user.full_name)}! Добро пожаловать в наш современный вейп-шоп 💨\n\n"
         "Нажми кнопку ниже, чтобы открыть магазин и сделать заказ.",
-        reply_markup=get_main_keyboard()
+        reply_markup=inline_kb
     )
 
 @start_router.message(F.text == "📜 История заказов")
