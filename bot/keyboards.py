d066cc45ffe01a5c4dc14c34f413fbf10367875f
Dmitriy Liakhovets <dmitriyliakhovets@MacBook-Air-Dmitriy.local>
2026-06-13 14:13:20 +0200

    Правим баги

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -4,7 +4,6 @@ from core.config import settings
 def get_main_keyboard() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="🛍️ Открыть магазин", web_app=WebAppInfo(url=settings.mini_app_url))],
             [KeyboardButton(text="📜 История заказов")]
         ],
         resize_keyboard=True,
